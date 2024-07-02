de4f1df1ee2238e43a012bab51fc2d18eaf15ea4
Park YuongHo <hexdrinker2020@gmail.com>
2024-07-02 20:51:53 +0900

    ps: 2024/07/02, 음양 더하기

---
+++ programmers/level-1/음양더하기.js
@@ -0,0 +1,9 @@
+/*
+  https://school.programmers.co.kr/learn/courses/30/lessons/76501
+*/
+function solution(absolutes, signs) {
+  return absolutes.reduce(
+    (acc, cur, idx) => (signs[idx] ? (acc += cur) : (acc -= cur)),
+    0
+  )
+}
