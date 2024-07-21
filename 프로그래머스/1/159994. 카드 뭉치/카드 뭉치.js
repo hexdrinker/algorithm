4d749611d4f2d619a82b97e1a9b68903939d8dc4
Park YoungHo <30437680+hexdrinker@users.noreply.github.com>
2024-07-21 20:47:37 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,16 +1,12 @@
-function solution(cards1, cards2, goal) {
-    var answer = '';
-    
-    while(goal.length) {
-        if (goal[0] === cards1[0]) {
+function solution(cards1, cards2, goal) {    
+    for (const s of goal) {
+        if (s === cards1[0]) {
             cards1.shift();
-            goal.shift();
-        } else if (goal[0] === cards2[0]) {
+        } else if (s === cards2[0]) {
             cards2.shift();
-            goal.shift();
         } else {
-            break;
+            return "No"
         }
     }
-    return !goal.length ? "Yes" : "No";
+    return "Yes"
 }
